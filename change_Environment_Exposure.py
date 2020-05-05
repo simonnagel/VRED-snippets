cfe56fa0395cc10e3ec2affee404184581d4a4ee
Simon Nagel <61537975+simonnagel@users.noreply.github.com>
2020-05-05 15:58:38 +0200

    Add files via upload

---
+++ change_Environment_Exposure.py
@@ -0,0 +1,18 @@
+'''
+DISCLAIMER:
+---------------------------------
+In any case, all binaries, configuration code, templates and snippets of this solution are of "work in progress" character.
+This also applies to GitHub "Release" versions.
+Neither Simon Nagel, nor Autodesk represents that these samples are reliable, accurate, complete, or otherwise valid. 
+Accordingly, those configuration samples are provided “as is” with no warranty of any kind and you use the applications at your own risk.
+'''
+
+
+
+#Change the value of the Exposure of an Environment via Python
+
+
+mat = findMaterial("Studio")
+
+
+vrFieldAccess(mat.fields().setReal32("exposure",3))
