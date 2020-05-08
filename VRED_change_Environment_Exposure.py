4c1d3d03c26f35ad65c72e1e56cf0afe3c6fd1e9
Simon Nagel <61537975+simonnagel@users.noreply.github.com>
2020-05-08 10:38:25 +0200

    Rename change_Environment_Exposure.py to VRED_change_Environment_Exposure.py

--- VRED_change_Environment_Exposure.py
+++ VRED_change_Environment_Exposure.py
@@ -4,7 +4,7 @@ DISCLAIMER:
 In any case, all binaries, configuration code, templates and snippets of this solution are of "work in progress" character.
 This also applies to GitHub "Release" versions.
 Neither Simon Nagel, nor Autodesk represents that these samples are reliable, accurate, complete, or otherwise valid. 
-Accordingly, those configuration samples are provided ìas isî with no warranty of any kind and you use the applications at your own risk.
+Accordingly, those configuration samples are provided ‚Äúas is‚Äù with no warranty of any kind and you use the applications at your own risk.
 '''
 
 
