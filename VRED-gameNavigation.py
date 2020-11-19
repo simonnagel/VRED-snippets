90bd23b3d0e79a71d348991ad65ab0013007abdc
Simon Nagel <61537975+simonnagel@users.noreply.github.com>
2020-11-19 08:55:43 +0100

    Update VRED-gameNavigation.py

--- VRED-gameNavigation.py
+++ VRED-gameNavigation.py
@@ -2,6 +2,9 @@
 #press Q and E to rotate
 #use mouse for change view
 
+# press M to make movement faster
+# press N to make movement slower
+
 # know bugs
 # cannot press Q,E at the same time as W,A,S,D
 # if pressed W too long and going "upwards" camera goes back on ground
@@ -10,10 +13,6 @@ import math
 import time
 from datetime import datetime
 
-# this example demonstrates how to force screen updates all the time
-
-# define class that calls a render update every frame
-
 
 class RenderAction(vrInteraction):
     def __init__(self):
@@ -21,35 +20,46 @@ class RenderAction(vrInteraction):
         self.addRender()
     def preRender(self):               
         
-        setHumanHeight()
+
         
         isKeyPressed()
         
-        #printTime()
 
-# create and activate render update object
 render = RenderAction()
 setNavigationSpeedMode(0)
 import keyboard
 from math import sqrt
 cam = getActiveCameraNode()
+speed = 0.1
 
-#dummy = findNode("dummy")
 
 def isKeyPressed():
     global cam
-    global camPos
-
+    global speed
     setNavigationSpeedMode(2)
     x = 0
     y = 0
-    speedForward = 0.1
-    speedSideward = 100
+    speedForward = speed
+    speedSideward = speedForward*2000
     setPanningSpeed(1)
     setDollySpeed(1)
-    active = 0    
-   
+    active = 0   
     
+    camPos = cam.getTranslation()
+    camPosWorld = cam.getWorldTransform()
+
+    #get the distance to ground
+    rayOrigin = Pnt3f(camPos[0],camPos[1],camPos[2])
+    rayDirection = Vec3f(0,0,-1)
+    intersection = getSceneIntersection(-1, rayOrigin, rayDirection)
+    interPos = intersection[1]
+    bestInterPos = interPos.z() 
+    collisionDistanceF = calcCollisionDistance(rayOrigin,camPos,camPosWorld,0,0,-1)
+    collisionDistanceR = calcCollisionDistance(rayOrigin,camPos,camPosWorld,1,0,0)
+    collisionDistanceL = calcCollisionDistance(rayOrigin,camPos,camPosWorld,-1,0,0)
+    #print collisionDistanceL
+    cam.setTranslation(camPos[0],camPos[1],bestInterPos+1800)   
+            
     if keyboard.is_pressed('q'):
         camRotFunc(5)
         active = 0
@@ -57,27 +67,33 @@ def isKeyPressed():
         camRotFunc(-5)
         active = 0     
     if keyboard.is_pressed('w'):
-
-        setCameraZoom(-speedForward)
-        y = 1
-        active = 1
+        if collisionDistanceF > 500:
+            setCameraZoom(-speedForward)
+            y = 1
+            active = 1
     if keyboard.is_pressed('s'):
 
         setCameraZoom(speedForward)
         y = -1
         active = 1
     if keyboard.is_pressed('a'):
-
-        setCameraPanning(speedSideward, 0)
-        x = 1
-        active = 1
+        if collisionDistanceL > 500:
+            setCameraPanning(speedSideward, 0)
+            x = 1
+            active = 1
     if keyboard.is_pressed('d'):
-
-        setCameraPanning(-speedSideward, 0)
-        x =-1
-        active = 1
+        if collisionDistanceR > 500:
+            setCameraPanning(-speedSideward, 0)
+            x =-1
+            active = 1
     if keyboard.is_pressed('x'):  
-        setCameraZoom(-10)    
+        setCameraZoom(-10)  
+        
+    if keyboard.is_pressed('m'):
+        adjustSpeed(1)
+    if keyboard.is_pressed('n'):
+        adjustSpeed(-1)            
+               
     if active == 1:
         setNavMode(3)
         setAllNavigationsEnabled(0)
@@ -87,47 +103,18 @@ def isKeyPressed():
         setAllNavigationsEnabled(1)
 
 
-def setHumanHeight():
-    global camPos
-    camPos = cam.getTranslation()
-    camPosWorld = cam.getWorldTransform
+def adjustSpeed(factor):
+    global speed
+    
+    speed = speed + 0.01* factor
+    if speed <0:
+        speed = 0
+    #print speed
 
-    #get the distance to ground
-    rayOrigin = Pnt3f(camPos[0],camPos[1],camPos[2])
-    rayDirection = Vec3f(0,0,-1)
-    intersection = getSceneIntersection(-1, rayOrigin, rayDirection)
-    interPos = intersection[1]
-    bestInterPos = interPos.z()
-    '''
-    x = (camPos[0], camPos[1], camPos[2])  
-      
-    #get the distance to front
-    v = Vec3f(0,0,-1)
-    rayDirectionFront = Vec3f(camPosWorld[0] * v.x() + camPosWorld[1] * v.y() + camPosWorld[2] * v.z() , camPosWorld[4] * v.x() + camPosWorld[5] * v.y() + camPosWorld[6] * v.z() ,camPosWorld[8] * v.x() + camPosWorld[9] * v.y() + camPosWorld[10] * v.z())
-    intersectionFront = getSceneIntersection(-1, rayOrigin, rayDirectionFront)
-    
-    interPosCollisionFront = intersectionFront[1]
-    y = (interPosCollisionFront.x(), interPosCollisionFront.y(), interPosCollisionFront.z())
-    collisionDistanceFront = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
-    #get the distance to back
-    rayDirectionBack = Vec3f(1,0,0)
-    intersectionBack = getSceneIntersection(-1, rayOrigin, rayDirectionBack)
-    interPosCollisionBack = intersectionBack[1]
-    y = (interPosCollisionBack.x(), interPosCollisionBack.y(), interPosCollisionBack.z())
-    collisionDistanceBack = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))     
-    rayDirectionLeft = Vec3f(0,1,0)
-    intersectionLeft = getSceneIntersection(-1, rayOrigin, rayDirectionLeft)
-    interPosCollisionLeft = intersectionLeft[1]
-    y = (interPosCollisionLeft.x(), interPosCollisionLeft.y(), interPosCollisionLeft.z())
-    collisionDistanceLeft = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))    
-    rayDirectionRight = Vec3f(0,-1,0)
-    intersectionRight = getSceneIntersection(-1, rayOrigin, rayDirectionRight)
-    interPosCollisionRight = intersectionRight[1]
-    y = (interPosCollisionRight.x(), interPosCollisionRight.y(), interPosCollisionRight.z())
-    collisionDistanceRight = math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))        
-            
-    print collisionDistanceFront
-    
+
+def getRayAverage():
+
+  '''
     interPosList = []
     for i in range(7):
         rayDirection = Vec3f(-(i-3)*0.1,0,-1)
@@ -145,8 +132,22 @@ def setHumanHeight():
         
     print mostFrequent(interPosList)
     '''
-    cam.setTranslation(camPos[0],camPos[1],bestInterPos+1800)
 
+def calcCollisionDistance(rayOriginF,camPos,camPosWorld,vx,vy,vz):
+
+    x = (camPos[0], camPos[1], camPos[2])  
+      
+    #get the distance to front
+    v = Vec3f(vx,vy,vz)
+    rayDirectionF = Vec3f(camPosWorld[0] * v.x() + camPosWorld[1] * v.y() + camPosWorld[2] * v.z() , camPosWorld[4] * v.x() + camPosWorld[5] * v.y() + camPosWorld[6] * v.z() ,camPosWorld[8] * v.x() + camPosWorld[9] * v.y() + camPosWorld[10] * v.z())
+    intersectionF = getSceneIntersection(-1, rayOriginF, rayDirectionF)
+    
+    interPosCollisionF = intersectionF[1]
+    y = (interPosCollisionF.x(), interPosCollisionF.y(), interPosCollisionF.z())
+    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
+    
+
+    
 
 def mostFrequent(List): 
     return max(set(List), key = List.count) 
@@ -159,7 +160,3 @@ def camRotFunc(rotDirection):
     cam.setRotation(camRot[0],camRot[1],camRot[2]+rotDirection)
         
  
-def printTime():
-    dt = datetime.now()
-    #print dt.second
-    #print dt.microsecond
